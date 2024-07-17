e5c92036ff5d638b55fba69c64d63e341b9ff958
Kurookku <Kodingu12345@gmail.com>
2024-07-17 12:39:45 -0400

    testing

--- src/TinkServer.lua
+++ src/TinkServer.lua
@@ -1,3 +1,67 @@
 local TinkServer = {}
 
+local function init1()
+    task.wait(1)
+    print("Started 1")
+end
+
+local function init2()
+    task.wait(2)
+    print("Started 2")
+end
+
+local function init3()
+    task.wait(3)
+    print("Started 3")
+end
+
+local function init4()
+    task.wait(4)
+    print("Started 4")
+end
+
+local function start()
+    local threads = {}
+    local mainThread = coroutine.running()
+    local mainThreadWaiting = false
+    local done = 0
+
+    local function report(fn)
+        return task.spawn(function()
+            -- Call the function:
+            local success, err = pcall(fn)
+            done += 1
+
+            -- Handle the error somehow:
+            if not success then
+                warn(err)
+            end
+
+            -- Resume main thread if needed:
+            if done == #threads and mainThreadWaiting then
+                task.spawn(mainThread)
+            end
+        end)
+    end
+
+    -- Start all threads:
+    table.insert(threads, report(init1))
+    table.insert(threads, report(init2))
+    table.insert(threads, report(init3))
+    table.insert(threads, report(init4))
+
+    -- It's possible that none of them yielded, thus all
+    -- are done immediately. But if one or more yielded,
+    -- we'll yield here too and wait.
+    if done ~= #threads then
+        mainThreadWaiting = true
+        coroutine.yield()
+        mainThreadWaiting = false
+    end
+
+    -- All done
+    print("Finished")
+end
+start()
+
 return TinkServer
